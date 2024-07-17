bca461e89128cacdde1fca26f6bf2deeaa1e1937
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-07-17 13:02:03 +0530

    Create ReverseWordsInAString.py

---
+++ day3/ReverseWordsInAString.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=s.split()
+        n=len(s)
+        rev=s[::-1]
+        stri=" ".join(rev)
+ 
+
+        
+        return stri
+        
