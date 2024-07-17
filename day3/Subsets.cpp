08f8869f13c165d26867f8f5d63ed36466371f1f
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-07-17 10:08:39 +0530

    Create Subsets.cpp

---
+++ day3/Subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void printsubsets(vector<int>& nums,vector<vector<int>>& result,int index,vector<int>& level){
+    
+    result.push_back(level);
+    for(int i=index;i<nums.size();i++){
+        if(i!=index && nums[i]==nums[i-1]){
+            continue;
+        }
+        level.push_back(nums[i]);
+        printsubsets(nums,result,i+1,level);
+        level.pop_back();
+
+    }
+    
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int>level;
+        printsubsets(nums,result,0,level);
+        return result;
+
+ 
+
+
+        
+    }
+};
