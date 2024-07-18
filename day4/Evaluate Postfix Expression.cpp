f1b6d965ae2df20e93a0dbd5d0c9bf3a6534b4a0
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-07-18 10:15:29 +0530

    Create Evaluate Postfix Expression.cpp

---
+++ day4/Evaluate Postfix Expression.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// C++ program to evaluate value of a postfix expression
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+
+int evaluatePostfix(string exp)
+{
+    stack<int> st;
+
+    for (int i = 0; i < exp.size(); ++i) {
+         
+
+        if (isdigit(exp[i]))
+            st.push(exp[i] - '0');
+ 
+        else {
+            int val1 = st.top();
+            st.pop();
+            int val2 = st.top();
+            st.pop();
+            switch (exp[i]) {
+            case '+':
+                st.push(val2 + val1);
+                break;
+            case '-':
+                st.push(val2 - val1);
+                break;
+            case '*':
+                st.push(val2 * val1);
+                break;
+            case '/':
+                st.push(val2 / val1);
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+    
+    cout<<obj.evaluatePostfix(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
