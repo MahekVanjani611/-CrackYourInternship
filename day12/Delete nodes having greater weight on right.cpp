8f83885cb0331e6d8edf640613c815868227faca
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-07-26 19:41:10 +0530

    Create Delete nodes having greater weight on right.cpp

---
+++ day12/Delete nodes having greater weight on right.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    Node *compute(Node *head)
+    {
+        if (head == NULL) return NULL;  
+        
+        vector<int> v;
+        Node* temp = head;
+ 
+        while (temp != NULL)
+        {
+            v.push_back(temp->data);
+            temp = temp->next;
+        }
+        
+       
+        deque<int> dq;
+        
+         int maxFromRight = INT_MIN;
+  
+        for (int i = v.size() - 1; i >= 0; --i)
+        {
+            if (v[i] >= maxFromRight)
+            {
+                dq.push_front(v[i]);
+                maxFromRight = v[i];
+            }
+        }
+
+        Node* dummy = new Node(-1);
+        Node* curr = dummy;
+        while (!dq.empty())
+        {
+            curr->next = new Node(dq.front());
+            dq.pop_front();
+            curr = curr->next;
+        }
+        
+        return dummy->next;
+    }
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
