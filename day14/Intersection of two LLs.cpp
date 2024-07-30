da0b4a4f97ba55349eb4b473b9397ee62a883747
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-07-30 08:56:44 +0530

    Update Intersection of two LLs.cpp

--- day14/Intersection of two LLs.cpp
+++ day14/Intersection of two LLs.cpp
@@ -1,1 +1,35 @@
 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* tempA=headA;
+        ListNode* tempB=headB;
+        while(tempA!=NULL&&tempB!=NULL)
+        {
+            if(tempA==tempB)
+            {
+                return tempA;
+            }
+            tempA=tempA->next;
+            tempB=tempB->next;
+            if(tempA==NULL)
+            {
+                tempA=headB;
+            }
+            else if(tempB==NULL)
+            {
+                tempB=headA;
+            }
+        }
+        return NULL;
+    }
+};
