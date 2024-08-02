9fc193a3f5d759f223c037ed215c6d02cbc6bdd7
Mahek <149080514+MahekVanjani611@users.noreply.github.com>
2024-08-02 23:51:57 +0530

    Create Range sum query-immutable.cpp

---
+++ day16/Range sum query-immutable.cpp
@@ -0,0 +1,18 @@
+class NumArray {
+public:
+vector<int> nums;
+    NumArray(vector<int>& nums) {
+        this->nums = nums;
+    }
+    
+    int sumRange(int left, int right) {
+       long long ans = 0;
+        
+        while(left<=right){
+            ans+=nums[left];
+            left++;
+        }
+
+        return ans;
+    }
+};
